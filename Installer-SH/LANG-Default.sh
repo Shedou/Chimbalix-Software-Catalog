ff019389246aad7cb188d938f5a507f980e6bfe0
Shedou <chimbal.chimbal@yandex.ru>
2024-11-13 13:15:11 +0400

    extra check LANG files

--- Installer-SH/LANG-Default.sh
+++ Installer-SH/LANG-Default.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
 Path_To_Script="$( dirname "$(readlink -f "$0")")"
-env LANG=C.UTF-8 "$Path_To_Script/install.sh"
+
+Exe_File_Name="$Path_To_Script/install.sh"
+
+if ! [[ -x "$Exe_File_Name" ]]; then
+	if ! chmod +x "$Exe_File_Name"; then echo " chmod error."; fi
+fi
+
+env LANG=C.UTF-8 "$Exe_File_Name"
