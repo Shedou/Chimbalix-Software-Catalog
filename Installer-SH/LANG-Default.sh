66a7094670547f3ae229f74a50c577374aab6617
Shedou <chimbal.chimbal@yandex.ru>
2024-11-13 13:53:32 +0400

    rename

--- Installer-SH/LANG-Default.sh
+++ Installer-SH/LANG-Default.sh
@@ -2,7 +2,7 @@
 
 Path_To_Script="$( dirname "$(readlink -f "$0")")"
 
-Exe_File_Name="$Path_To_Script/install.sh"
+Exe_File_Name="$Path_To_Script/installer.sh"
 
 if ! [[ -x "$Exe_File_Name" ]]; then
 	if ! chmod +x "$Exe_File_Name"; then echo " chmod error."; fi
